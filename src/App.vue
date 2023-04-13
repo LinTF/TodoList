355a80e342ae5ea108c9624a3087e03c5be91c96
Iris <lin.tf1124@gmail.com>
2023-04-14 02:19:47 +0800

    add 新增待辦事項區塊

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
-  
+  <div id="edit-block">
+    <div class="row justify-content-md-center">
+      <div class="col-auto">
+        <label for="todoItem" class=" col-form-label">請輸入待辦事項：</label>
+      </div>
+      <div class="col-md-6">
+        <input type="text" class="form-control" id="todoItem" placeholder="請輸入待辦事項">
+      </div>
+      <div class="col-auto">
+        <button type="submit" class="btn btn-primary mb-3">新增</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 
 </script>
 
-<style>
-
+<style scoped lang="scss">
+  #edit-block {
+    margin-top: 20px;
+  }
 </style>
