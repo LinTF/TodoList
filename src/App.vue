4790289bb4dd7202e3d79934a45c5a70eb2349ca
Iris <lin.tf1124@gmail.com>
2023-04-14 23:53:34 +0800

    可以新增待辦事項到待辦區

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,41 @@
   <div id="edit-block">
     <div class="row justify-content-md-center">
       <div class="col-auto">
-        <label for="todoItem" class=" col-form-label">請輸入待辦事項：</label>
+        <label for="todoItem" class=" col-form-label">待辦事項：</label>
       </div>
       <div class="col-md-6">
-        <input type="text" class="form-control" id="todoItem" placeholder="請輸入待辦事項">
+        <input type="text" class="form-control" id="todoItem" placeholder="請輸入待辦事項" v-model="todoItemText">
       </div>
       <div class="col-auto">
-        <button type="submit" class="btn btn-primary mb-3">新增</button>
+        <button type="submit" class="btn btn-primary mb-3" @click="addTodoItem">新增</button>
       </div>
     </div>
+  </div>
+  <div>
+    <p v-for="(item, index) in todoItem" :key="index">{{ item }}</p>
   </div>
 </template>
 
 <script>
-
+  export default {
+    name: 'todo',
+    data() {
+      return {
+        todoItem: [],
+        ingItem: [],
+        finishItem: [],
+        todoItemText: ''
+      }
+    },
+    methods: {
+      addTodoItem() {
+        this.todoItem.push(this.todoItemText);
+        
+        // 新增後還原 textbox 為空值
+        this.todoItemText='';
+      }
+    }
+  }
 </script>
 
 <style scoped lang="scss">
